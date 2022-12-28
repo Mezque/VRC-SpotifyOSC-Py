5b5d227f5287268b2807344fa2155dc2f4cc7a91
Micah <31026406+Mezque@users.noreply.github.com>
2022-12-28 08:25:55 -0400

    Code updates:
    1: Optimization's done to the amounts of Spotify API calls, all function in relation to the current playing song is done through a single request now.
    
    2: Moved loop onto its own thread to see if this adds any sort of optimization to the program, I assume it won't really change much but I do believe its better practice?
    
    3: Made a pref INI to be able to toggle constant sending of the current track to the chatbox OSC endpoint, this mode also includes a timestamp of where the user is scrobbled into the song and its relation to the songs total length (currently if the seconds are >9 it will display as [ MIN:9 ] instead of [ MIN:09 ] ( there is no 0 prefixing the second ) I will look into a way to change this behaviour. ( this is also where the new dependency in requirements.txt comes from )
    
    to do: move client secrets into their own INI file and then prompt the user to input it upon first run then save their input to make it easier for the end user to use the product.
    
    going to keep messing around and see if there are ways to optimize code more.

--- Py/Spotify/Spotify.py
+++ Py/Spotify/Spotify.py
@@ -2,58 +2,97 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from pythonosc.udp_client import SimpleUDPClient
 import time
+import configparser
+import os.path
+import threading
 
 client = SimpleUDPClient("127.0.0.1", 9000)
+config = configparser.ConfigParser()
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="", #put your client id here
                                                client_secret="", #put your client secret here
                                                redirect_uri="http://localhost:8888/spotify/callback",
                                                scope="user-read-currently-playing")) # You can find out more about what this means here, https://developer.spotify.com/documentation/general/guides/authorization/scopes/ basically it allows the program to read your current playing song, but no other control over your spotify account.
-Song = ["", True]
+Song1 = ["", True]
 # 1: Go to https://developer.spotify.com/dashboard/ and log in with your Spotify account and then create yourself a new app. 
 # 2: You will be redirected to the dashboard of your app, copy the client id and client secret (press Show Client Secret) and paste them in the apove string feilds as discribed.
 # !NOTE!: Always store the client secret key securely; never reveal it publicly! If you suspect that the secret key has been compromised, regenerate it immediately by clicking the ROTATE button on the app overview page!!!
 # It is time to configure our app. Click on Edit Settings and find "Redirect URLs", type http://localhost:8888/spotify/callback in the feild.
 # Refrence: https://developer.spotify.com/documentation/general/guides/authorization/app-settings/ 
 
+def TimeConversion(time):
+    seconds, time = divmod(time, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    return seconds, minutes
+
 def DisplayName():
     return sp.current_user()['display_name'] #returns the display name of the user.
-def CurrentSong():
-    CurSong = sp.current_user_playing_track() 
+     
+def get_current_song_and_artist():
+    CurSong = sp.current_user_playing_track()
     if CurSong is None:
         print("No song is currently playing")
-        return None
-    else: 
+        return None, None
+    else:
         Song = CurSong["item"]["name"]
-        return Song 
-def CurrentArtist():
-    CurSong = sp.current_user_playing_track() 
-    if CurSong is None:
-        print("No song is currently playing")
-        return None
-    else: 
-        Artist = CurSong['item']['artists'][0]['name'] # https://stackoverflow.com/a/63907495 the [0]['name'] part fixed it :)
-        return Artist
+
+        Artist = CurSong['item']['artists'][0]['name']
+
+        Length = CurSong['item']["duration_ms"]
+        sec,min = TimeConversion(int(Length))
+        song_lenth=("{0}:{1}".format(min,sec))
+
+        Position = CurSong["progress_ms"]
+        sec,min = TimeConversion(int(Position))
+        song_pos=("{0}:{1}".format(min,sec))
+
+        return (Song, Artist, song_lenth, song_pos)
+
+def Prefs():
+    exist = os.path.exists("Settings/settings.ini")
+    if exist == True:
+        config.read('Settings/settings.ini')
+    else:
+        print("Prefs file doesn't exist, creating now.")
+        os.mkdir("Settings")
+        writeNewFile = open('Settings/settings.ini', 'w')
+        writeNewFile.write('[Preferences]\nKeepSendingOSC=true')
+        writeNewFile.close
+
 def loop():
     prev_song = ""
-    client = SimpleUDPClient("127.0.0.1", 9000)
     while True:
-        cur_song = CurrentSong()
-        cur_artist = CurrentArtist()
+        song,artist,song_lenth,song_pos = get_current_song_and_artist()
+        cur_song = song
+        cur_artist = artist
+        KeepSendingOSC = config.getboolean('Preferences', 'KeepSendingOSC')
+        if KeepSendingOSC == True:
+            if cur_song != prev_song:
+                print("- Currently Playing:", cur_song, "by", cur_artist)
+                prev_song = cur_song
+            time1 = song_lenth
+            time2 = song_pos
+            Song1[0] = f"Now playing {cur_song} by {cur_artist} {time2}/{time1}"
+            client.send_message("/chatbox/input",Song1) #want to clean this up with threading but it was working not as expected, will look more into later after getting the feature out in the first place.
+        time.sleep(2)
+
         if cur_song != prev_song:
             print("- Currently Playing:", cur_song, "by", cur_artist)
-            Song[0] = f"Now playing {cur_song} by {cur_artist}"
-            client.send_message("/chatbox/input",Song) #/chatbox/input 
+            Song1[0] = f"Now playing {cur_song} by {cur_artist}"
+            client.send_message("/chatbox/input",Song1) #/chatbox/input 
             prev_song = cur_song
         time.sleep(2) # pause for 2 seconds before checking again
 
 def main():
+    song,artist,song_lenth,song_pos = get_current_song_and_artist()
     print("--------------------------------")
     print("- Starting Spotify API Python  -")
     print("- Current User:", DisplayName())
-    print("- Currently Playing:", CurrentSong(), 'by', CurrentArtist())
+    print("- Currently Playing:", song, 'by', artist)
     print("--------------------------------")
-    loop()
+    Prefs()
+    thread = threading.Thread(target=loop)
+    thread.start()
     
 main()
 if __name__ == "__main__":
